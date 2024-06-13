b764118bda0ad81acf42c31daa3d2ba4024766f5
Mulla20 <ikraammulla20@gmail.com>
2024-06-13 01:19:38 -0400

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        //Project was created to practice our basics to solidify our Java Concepts
+
+        //Print
+        System.out.println('T'); //Character print
+        System.out.println("This is a print statement for a String"); //String Print S.
+        System.out.println("This is a print statement of a # " + 5 ); //Print a #
+        System.out.println( 1.19775); //Print decimal
+        System.out.println( );
+        System.out.println("this is the new commit");
+
+
+
     }
 }
